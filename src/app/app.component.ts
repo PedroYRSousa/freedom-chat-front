022ba2de2231387d64ae7c3c209df0d19a791c19
yagosousa2512 <yagosousa2512@gmail.com>
2022-08-15 12:25:23 +0000

    Entrykey added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,18 @@ export class AppComponent {
 
   public get Id() { return (this.chatService.Id || "ID"); };
   public get Connected() { return (this.chatService.Connected); };
+  protected get ContactIsOffline(): boolean { return (this.chatService.ContactIsOffline); }
+  protected set EntryKey(entryKey: string) {
+    if (this.chatService.ChatSelected === undefined)
+      return;
+
+    this.chatService.ChatSelected.crypto.EntryKey = entryKey;
+  }
+  protected get EntryKey() {
+    if (this.chatService.ChatSelected === undefined)
+      return "";
+
+    return (this.chatService.ChatSelected.crypto.EntryKey);
+  }
+
 }
